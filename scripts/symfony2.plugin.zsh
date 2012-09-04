11bc5e61a6df9077abc2fa7109289a644724d483
Martin Richard <martius@martiusweb.net>
2012-09-04 14:50:12 +0200

    Symfony2 plugin
    Signed-off-by: Martin Richard <martius@martiusweb.net>

---
+++ scripts/symfony2.plugin.zsh
@@ -0,0 +1,15 @@
+# Symfony2 basic command completion
+SF2_ROOT_DIR=~/Code/jolitv/secam
+
+_symfony2_get_command_list () {
+	php $SF2_ROOT_DIR/app/console --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
+}
+
+_symfony2 () {
+  if [ -f $SF2_ROOT_DIR/app/console ]; then
+    compadd `_symfony2_get_command_list`
+  fi
+}
+
+compdef _symfony2 app/console
+compdef _symfony2 sf2
