5e3546572740007828a8b9e69e3fa3d9ed3a782f
Martin Richard <martius@martiusweb.net>
2013-11-03 16:54:36 +0100

    Fixed a bug expanding '' in command line when parsing term title
    Signed-off-by: Martin Richard <martius@martiusweb.net>

--- scripts/title.zsh
+++ scripts/title.zsh
@@ -15,25 +15,43 @@
 
 # title
 #   sets terminal title
-# based on http://github.com/MrElendig/dotfiles-alice/blob/master/.zshrc
+# based on https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/termsupport.zsh
 
-case $TERM in
-  *xterm*|rxvt|rxvt-unicode|rxvt-256color|rxvt-unicode-256color|dtterm|kterm|Eterm)
-    precmd () {
-      print -Pn "\e]0;%n@%M %~\a"
-    }
-    preexec () {
-      print -Pn "\e]0;%n@%M %~ ($1)\a"
-    }
-  ;;
-  screen)
-    precmd () {
-      print -Pn "\e]83;title \"$1\"\a"
-      print -Pn "\e]0;$TERM - (%L) %n@%M %~\a"
-    }
-    preexec () {
-      print -Pn "\e]83;title \"$1\"\a"
-      print -Pn "\e]0;$TERM - (%L) %n@%M %~ ($1)\a"
-    }
-  ;;
-esac
+# title short_title long_title
+function title {
+  if [[ "$DISABLE_AUTO_TITLE" == "true" ]] || [[ "$EMACS" == *term* ]]; then
+    return
+  fi
+
+  case $TERM in
+    *xterm*|rxvt|rxvt-unicode|rxvt-256color|rxvt-unicode-256color|dtterm|kterm|Eterm)
+      print -Pn "\e]2;$2:q\a" #set window name
+      print -Pn "\e]1;$1:q\a" #set icon (=tab) name
+    ;;
+
+    screen|screen-256color)
+      print -Pn "\ek$1:q\e\\"
+    ;;
+  esac
+}
+
+# Appears when you have the prompt
+function termtitle_precmd {
+  title "%15<..<%~%<<" "%n@%m: %~"
+}
+
+# Appears at the beginning of (and during) of command execution
+function termtitle_preexec {
+  emulate -L zsh
+  setopt extended_glob
+
+  # cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD=${1[(wr)^(*=*|sudo|ssh|rake|-*)]}
+  local LINE="${2:gs/$/\\$}"
+  LINE="${LINE:gs/%/%%}"
+  title "$CMD" "%100>...>$LINE%<<"
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd termtitle_precmd
+add-zsh-hook preexec termtitle_preexec
